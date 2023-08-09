64d907f1f3dbc0097c4b45bc4863478628b3eb28
ManoMax <gabriel.matos@ccc.ufcg.edu.br>
2023-08-09 10:10:17 -0300

    refactor: more roles added to user and default role changed

--- src/modules/User/User.js
+++ src/modules/User/User.js
@@ -1,33 +1,35 @@
-const { Schema, model } = require('mongoose');
-const Ej = require('@ej/Ej');
+const { Schema, model } = require("mongoose");
+const Ej = require("@ej/Ej");
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        enum: ['presidente', 'diretor', 'gerente'],
-        required: true,
-        default: 'diretor'
+      type: String,
+      enum: ["Presidente", "Diretor(a)", "Assessor(a)", "Conselheiro(a)"],
+      required: true,
+      default: "Assessor(a)",
     },
     ej: {
-        type: Schema.Types.ObjectId,
-        ref: Ej,
-        required: true
-    }
-},
-    {
-        timestamps: true,
-    });
+      type: Schema.Types.ObjectId,
+      ref: Ej,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = model('User', UserSchema);
+module.exports = model("User", UserSchema);
