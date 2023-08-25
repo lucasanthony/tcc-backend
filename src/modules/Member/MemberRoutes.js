9fa9fc333f2e32c1a0388821fee746272e43a0ac
FLuiz22 <filipeluiz39@gmail.com>
2023-08-25 20:55:36 -0300

    Refactor: autorizeUser changed to autorizeLeadership

--- src/modules/Member/MemberRoutes.js
+++ src/modules/Member/MemberRoutes.js
@@ -5,6 +5,6 @@ const { authorizeUser, authorizeLeadership } = require('@middlewares/auth')
 router.post('/member', authorizeLeadership, save);
 router.get('/member', authorizeUser, findByEj);
 router.patch('/member/:id', authorizeUser, update);
-router.delete('/member/:id', authorizeUser, remove);
+router.delete('/member/:id', authorizeLeadership, remove);
 
 module.exports = router;
