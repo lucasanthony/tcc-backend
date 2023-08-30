7483009872717e6337daf5bb065fed1bdc875a79
FLuiz22 <filipeluiz39@gmail.com>
2023-08-30 20:07:08 -0300

    Refactor: DTO projects added

--- src/modules/Project/ProjectService.js
+++ src/modules/Project/ProjectService.js
@@ -16,7 +16,7 @@ module.exports = {
 			customer
 		})
 
-		return project;
+		return getDTOproject(project);
 	},
 
 	// only for test purposes
@@ -24,16 +24,35 @@ module.exports = {
 		// const ejs = await Ej.find().populate({ path: 'president', select: 'name -_id' });
 		const projects = await Project.find({ ej: ejId }).populate({ path: 'team', select: 'name role -_id' });;
 
-		return projects;
+		const projectsDTO = projects.map((project) => {
+			return getDTOproject(project);
+		});
+
+		return projectsDTO;
 	},
 
 	async remove(projectId) {
 		const project = await Project.deleteOne({ _id: projectId });
-		return project;
+		return getDTOproject(project);
 	},
 
 	async update(projectId, data) {
 		const updatedProject = await Project.findOneAndUpdate({ _id: projectId }, data)
-		return updatedProject
+		return getDTOproject(updatedProject);
 	}
+}
+
+function getDTOproject(project) {
+	return {
+		_id: project._id,
+		ej: project.ej,
+		name: project.name,
+		description: project.description,
+		tags: project.tags,
+		team: project.team,
+		startDate: project.startDate,
+		finishDate: project.finishDate,
+		contractLink: project.contractLink,
+		customer: project.customer
+	};
 }
