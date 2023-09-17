dff42edfbf4e318441f710846e683f5d63e2e2fa
anaritamed <ana.rita.medeiros.souza@ccc.ufcg.edu.br>
2023-09-17 14:41:38 -0300

    refactor: creates NewsSchema structure and organization

---
+++ src/modules/News/News.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+const Project = require("@project/Project");
+
+const NewsSchema = new Schema({
+    project: {
+        type: String,
+        ref: Project,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    images: [{
+        type: String,
+        required: false 
+    }],
+    updateLink: [{
+        type: String,
+        required: false
+    }]
+},
+    {
+        timestamps: true,
+    });
+
+module.exports = model("News", NewsSchema);
