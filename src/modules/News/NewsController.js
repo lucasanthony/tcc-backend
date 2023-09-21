6f2e84a6c7cdeda7f62066abb2a51f502b2c4e3d
anaritamed <ana.rita.medeiros.souza@ccc.ufcg.edu.br>
2023-09-20 22:14:40 -0300

    feat: create save and remove functions on NewsController

---
+++ src/modules/News/NewsController.js
@@ -0,0 +1,21 @@
+const { save, remove } = require("./NewsService");
+
+module.exports = {
+    async save(req, res) {
+        try {
+            const news = await save(req.body, req.ejId);
+            return res.status(201).send({ news: news });
+        } catch (error) {
+            return res.status(500).send({ error: error.message });
+        }
+    },
+
+    async remove(req, res) {
+        try {
+            const removedNews = await remove(req.params.id);
+            return res.status(200).send({ news: removedNews, message: "Atualização do projeto removida com sucesso!" });
+        } catch (error) {
+            return res.status(500).send({ error: error.message });
+        }
+    }
+}
