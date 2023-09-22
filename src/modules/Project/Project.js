512fd0312bf3088e38f6589c96d453a541c38592
ManoMax <gabriel.matos@ccc.ufcg.edu.br>
2023-09-22 11:58:25 -0300

    refactor: string value to ref of news in Project Model

--- src/modules/Project/Project.js
+++ src/modules/Project/Project.js
@@ -3,65 +3,80 @@ const Ej = require("@ej/Ej");
 const Member = require("@member/Member");
 const News = require("@news/News");
 
-const ProjectSchema = new Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	description: {
-		type: String,
-		required: true
-	},
-	tags: [{
-		type: String,
-		enum: ["Backend", "Frontend", "Mobile", "Wordpress", "Assessoria", "Treinamento"],
-		required: false
-	}],
-	ej: {
-		type: Schema.Types.ObjectId,
-		ref: Ej,
-		required: true
-	},
-	team: [{
-		type: Schema.Types.ObjectId,
-		ref: Member,
-		required: false
-	}],
-	startDate: {
-		type: Date,
-		required: true
-	},
-	finishDate: {
-		type: Date,
-		required: false
-	},
-	contractLink: {
-		type: String,
-		required: false
-	},
-	customer: {
-		email: {
-			type: String,
-			required: false
-		},
-		contact: {
-			type: String,
-			required: false
-		},
-		name: {
-			type: String,
-			required: false
-		}
-	},
-	news: [{
-		type: Schema.Types.ObjectId,
-		ref: News,
-		required: false,
-		default: []
-	}]
-},
-	{
-		timestamps: true,
-	});
+const ProjectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tags: [
+      {
+        type: String,
+        enum: [
+          "Backend",
+          "Frontend",
+          "Mobile",
+          "Wordpress",
+          "Assessoria",
+          "Treinamento",
+        ],
+        required: false,
+      },
+    ],
+    ej: {
+      type: Schema.Types.ObjectId,
+      ref: Ej,
+      required: true,
+    },
+    team: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Member,
+        required: false,
+      },
+    ],
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    finishDate: {
+      type: Date,
+      required: false,
+    },
+    contractLink: {
+      type: String,
+      required: false,
+    },
+    customer: {
+      email: {
+        type: String,
+        required: false,
+      },
+      contact: {
+        type: String,
+        required: false,
+      },
+      name: {
+        type: String,
+        required: false,
+      },
+    },
+    news: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'News',
+        required: false,
+        default: [],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("Project", ProjectSchema);
