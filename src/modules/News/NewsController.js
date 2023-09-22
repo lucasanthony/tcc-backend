3b794f30669d8016c4528cc6c872755ef748e4b4
ManoMax <gabriel.matos@ccc.ufcg.edu.br>
2023-09-22 12:12:00 -0300

    refactor: params and some environments changed

--- src/modules/News/NewsController.js
+++ src/modules/News/NewsController.js
@@ -3,7 +3,7 @@ const { save, findByProject, remove, update } = require("./NewsService");
 module.exports = {
     async save(req, res) {
         try {
-            const news = await save(req.body, req.params.project-id);
+            const news = await save(req.memberId, req.body, req.params.projectId);
             return res.status(201).send({ news: news });
         } catch (error) {
             return res.status(500).send({ error: error.message });
@@ -12,29 +12,28 @@ module.exports = {
 
     async findByProject(req, res) {
         try {
-            const allNews = await findByProject(req.params.project-id);
-            return res.status(201).send({ news: news });
-        } catch (error) {
-            return res.status(500).send({ error: error.message });
-        }
-    },
-
-    async remove(req, res) {
-        try {
-            const removedNews = await remove(req.body.newsId, req.params.project-id);
-            return res.status(200).send({ news: removedNews, message: "Atualização do projeto removida com sucesso!" });
+            const allNews = await findByProject(req.params.projectId);
+            return res.status(201).send({ news: allNews });
         } catch (error) {
             return res.status(500).send({ error: error.message });
         }
     },
 
     async update(req, res) {
+      try {
+          const updatedNews = await update(req.params.newsId, req.body);
+          return res.status(200).send({ news: updatedNews, message: 'Atualização do projeto editada com sucesso!' });
+      } catch (error) {
+          return res.status(500).send({ error: error.message });
+      }
+    },
+
+    async remove(req, res) {
         try {
-            const updatedNews = await update(req.params.news-id, req.body);
-            return res.status(200).send({ news: updatedNews, message: 'Atualização do projeto editada com sucesso!' });
+            const newsWithoutRemovedNews = await remove(req.params.projectId, req.body);
+            return res.status(200).send({ news: newsWithoutRemovedNews, message: "Atualização do projeto removida com sucesso!" });
         } catch (error) {
             return res.status(500).send({ error: error.message });
         }
-    },
-    
+    },    
 }
